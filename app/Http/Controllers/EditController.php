0388a29f9779ca69056d67e707db3f47c01d0285
Diego Hincapie <dfhincapiem@unal.edu.co>
2018-04-01 00:31:07 -0500

    Arreglos generales

--- app/Http/Controllers/EditController.php
+++ app/Http/Controllers/EditController.php
@@ -14,6 +14,11 @@ class EditController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $user = Auth::user();
